1f8cd028bc476324c2c3ec9c4320e3d837f3b7d1
C00lplayer <bibidhkafle@icloud.com>
2024-06-19 11:06:26 +1000

    Added comments and fixed formatting of code

--- Calculating Prices.py
+++ Calculating Prices.py
@@ -1,12 +1,14 @@
 import json
 import csv
 
+
+# Opening the file containing all the plan information
 with open('Electricity-plans.json','r') as f:
     data = json.load(f)
 
 
 
-# Code for outputting and calculating monthly cost:
+# This code below calculates the monthly cost for each plan and prints it in a table-like format:
 """
 print(f"{'Provider':<20} {'Specific Plan Name':<60} {'Estimated Monthly Price':<30} {'Upfront Credit':<20} {"Link":<30}")
 print("-"*170)
@@ -16,22 +18,28 @@ DAYS_IN_A_MONTH= 30
 
 for each_provider in data:
     Name = each_provider["Provider Name"]
+    
     for plan in each_provider["Plans"]:
+        
         Plan_Name = plan["Plan Name"]
         General_Usage = (Monthly_Usage* float(plan['General Usage Charge']))/100
         Supply_Charge = (DAYS_IN_A_MONTH*float(plan['Supply Charge']))/100
         Estimated_Monthly_Price = "$" + str(round(General_Usage + Supply_Charge,2))
         Upfront_Credit =  "$" + plan["Upfront Credit"]
+        
         try:
             Link = plan["Plan Link"]
         except KeyError:
             Link = 'Error: No Link Found'
+        
         print(f"{Name:<20} {Plan_Name:<60} {Estimated_Monthly_Price:<30} {Upfront_Credit:<20} {Link:<30}")
 
 with open("Table-of-Electricity-Prices.json", 'r'):
 """
 
-# Code for outputting and calculating monthly cost which is sorted by cheapest first:
+
+
+# This code below calculates the monthly cost for each plan and prints it in a table-like format sorted by the cheapest plan and the using the upfront credit as a tiebreaker:
 """
 All_Plans = []
 Monthly_Usage = 282.162
@@ -39,56 +47,72 @@ DAYS_IN_A_MONTH= 30
 
 for each_provider in data:
     Name = each_provider["Provider Name"]
+    
     for plan in each_provider["Plans"]:
         Plan_info = {}
         Plan_info["Provider"] = Name
         Plan_info["Plan Name"] = plan["Plan Name"]
+        
         General_Usage = (Monthly_Usage* float(plan['General Usage Charge']))/100
         Supply_Charge = (DAYS_IN_A_MONTH*float(plan['Supply Charge']))/100
         Plan_info["Estimated Monthly Price"] = "$" + str(round(General_Usage + Supply_Charge,2))
+        
         Plan_info["Upfront Credit"] =  "$" + plan["Upfront Credit"]
+        
         try:
             Plan_info["Link"] = plan["Plan Link"]
         except KeyError:
             Plan_info["Link"] = 'Error: No Link Found'
+        
         All_Plans.append(Plan_info)
+
 All_Plans = sorted(All_Plans, key = lambda x: (float(x["Estimated Monthly Price"][1:]),int(x["Upfront Credit"][1:])))
 
 print(f"{'Provider':<20} {'Specific Plan Name':<60} {'Estimated Monthly Price':<30} {'Upfront Credit':<20} {"Link":<30}")
 print("-"*170)
-#print(All_Plans)
+
 for plan in All_Plans:
         Prover_Name = plan["Provider"]
         Plan_Name = plan["Plan Name"]
         Estimated_Monthly_Price = plan["Estimated Monthly Price"]
         Upfront_Credit = plan["Upfront Credit"]
         Link = plan["Link"]
+        
         print(f"{Prover_Name:<20} {Plan_Name:<60} {Estimated_Monthly_Price:<30} {Upfront_Credit:<20} {Link:<30}")
 """
 
 
+
+# This code below calculates the monthly cost for each plan, sorts the plans by the cheapest plan and the using the upfront credit as a tiebreaker and then finally saves it as a CSV file:
 All_Plans = []
 Monthly_Usage = 282.162
 DAYS_IN_A_MONTH= 30
+
 csv_file = open("sorted-monthly-plans.csv","w")
 writer = csv.writer(csv_file)
 
 
 for each_provider in data:
     Name = each_provider["Provider Name"]
+    
     for plan in each_provider["Plans"]:
         Plan_info = {}
         Plan_info["Provider"] = Name
         Plan_info["Plan Name"] = plan["Plan Name"]
+        
         General_Usage = (Monthly_Usage* float(plan['General Usage Charge']))/100
         Supply_Charge = (DAYS_IN_A_MONTH*float(plan['Supply Charge']))/100
         Plan_info["Estimated Monthly Price"] = "$" + str(round(General_Usage + Supply_Charge,2))
+        
         Plan_info["Upfront Credit"] =  "$" + plan["Upfront Credit"]
+        
         try:
             Plan_info["Link"] = plan["Plan Link"]
         except KeyError:
             Plan_info["Link"] = 'Error: No Link Found'
+        
         All_Plans.append(Plan_info)
+
 All_Plans = sorted(All_Plans, key = lambda x: (float(x["Estimated Monthly Price"][1:]),int(x["Upfront Credit"][1:])))
 
 writer.writerow(['Provider','Specific Plan Name', 'Estimated Monthly Price', 'Upfront Credit',"Link"])
@@ -99,9 +123,8 @@ for plan in All_Plans:
         Estimated_Monthly_Price = plan["Estimated Monthly Price"]
         Upfront_Credit = plan["Upfront Credit"]
         Link = plan["Link"]
+        
         writer.writerow([Prover_Name, Plan_Name, Estimated_Monthly_Price, Upfront_Credit, Link])
 
 csv_file.close()
 
-#with open("sorted-monthly-plans.csv","r") as csv_file:
-     #print(csv_file.read())
--- Collecting Base Data.py
+++ Collecting Base Data.py
@@ -17,31 +17,45 @@ def open_html(path):
         return f.read()
 
 
-#url = "https://wattever.com.au/compare-best-electricity-rates-vic/#powercor"
+# URL in which the data is do be extracted from
+url = "https://wattever.com.au/compare-best-electricity-rates-vic/#powercor"
 
-#r = requests.get(url)
+# Extracts the code for the data to be use
+r = requests.get(url)
 
-#save_html(r.content,"energy_data1")
+# Saves the webpage code locally so that you can access it multiple times when testing the code so that our data extraction doesn't impact the website
+save_html(r.content,"energy_data1")
 
+# NOTE: these 3 lines of code above only need to be executed once as once you save the webpage there is no need to save it again apart from updating values that exist inside the table
+
+# Opens the code for the webpage that was saved locally
 html = open_html("energy_data1")
 
-
+# Pass the webpage code through BeautifulSoup module so that it returns an object that will be easy to navigate through
 soup = BeautifulSoup(html,'html.parser')
 data = []
 
+# Selecting the specific portion of the HTML in which the table of data exists
 rows = soup.select('#table_8 tbody tr')
 
+# Going through each row of the table and saving the required information
 for i in rows:
     d = {}
     info= i.select('td')
     d["Provider Name"] = info[2].text
-    print(d["Provider Name"])
     d['Plans'] = []
+    # As the table also includes a link to further electricity plans, it is saved so that we can scrap that too
     Specific_Plan_Link = "https://wattever.com.au" + info[7].select_one('a')['href']
+    
+    # Using the link that includes the additional plans, we use the request and BeautifulSoup module to load up that webpage
     plan_link = requests.get(Specific_Plan_Link)
     soup1 = BeautifulSoup(plan_link.content,'html.parser')
+    
+    # Selecting the specific portion of the HTML in which the table of data exists
     plan_rows= soup1.select('.ue-content-text')
-    print("inside")
+    
+    # The previous selection made doesn't narrow the code enough so we go through each table to find the specific one we are looking for
+    # which in this case is one about 'Powercor'
     for i in plan_rows:
         h3_tag = i.select_one('h3')
         if h3_tag is not None: 
@@ -49,6 +63,7 @@ for i in rows:
                 plan_rows= i.select('tbody tr')
                 break
     
+    # Now we go through each row and extract the specific data about the plans we need
     for j in plan_rows:
         d1 = {}
         plan_info = j.select('td')
@@ -64,10 +79,13 @@ for i in rows:
             except:
                 pass
             d['Plans'].append(d1)
+    
+    # We now append the different plans to the main list of data only if the plans exist
     if d['Plans']:
         data.append(d)
 
 
+# Saving the plan data locally so that to analyse the data we don't need to repeatedly scrape the page
 with open('Electricity-plans.json','w') as f:
     json.dump(data,f)
 
