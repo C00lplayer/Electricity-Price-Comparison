c1ad57f92c7fd9e677b6de21976901a1a7323a19
C00lplayer <bibidhkafle@icloud.com>
2024-06-28 10:09:17 +1000

    Changed graph and variable names

--- Analysis of Plans.py
+++ Analysis of Plans.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 markers =['X','^','o','s','p','*','D','P','H','.']
 Title_font = {'family':'sans-serif','size':30}
 Body_font = {'family':'sans-serif','size':20}
-PAYONTIMEDISCOUNT = 0.7
-Current_General = 0.38874
-Current_Supply = 1.87924
-Current_usage = {"Aug 2023": 300.745, "Sep 2023": 235.27,"Oct 2023": 226.827, "Nov 2023": 184.793, "Dec 2023": 85.804,"Jan 2024":248.313, "Feb 2024": 256.431, "Mar 2024": 238.438, "Apr 2024":282.162, "May 2024":444.133}
+
+# Enter you plan details below:
+PAYONTIMEDISCOUNT = "Enter current pay on time discount here"
+Current_General = "Enter Current general usage charge here"
+Current_Supply =  "EnterCurrent supply charge here"
+Current_usage = {"Aug 2023": "Enter usage in KWH" , "Sep 2023": "Enter usage in KWH" ,"Oct 2023": "Enter usage in KWH" , "Nov 2023": "Enter usage in KWH", "Dec 2023": "Enter usage in KWH" ,"Jan 2024":"Enter usage in KWH", "Feb 2024": "Enter usage in KWH", "Mar 2024": "Enter usage in KWH", "Apr 2024": "Enter usage in KWH", "May 2024": "Enter usage in KWH"}
 Periods = {"Aug 2023": 31, "Sep 2023": 30,"Oct 2023": 31, "Nov 2023": 30, "Dec 2023": 31,"Jan 2024":31, "Feb 2024": 29, "Mar 2024": 31, "Apr 2024":30, "May 2024":31}
 
 
@@ -15,8 +17,8 @@ Dates = Periods.keys()
 current_cost = []
 for i in Dates:
             days = Periods[i]
-            usage = Current_usage[i]
-            current_cost.append(round(((Current_General*usage)+(days*Current_Supply))*PAYONTIMEDISCOUNT,2))
+            usage = CURRENT_USAGE[i]
+            current_cost.append(round(((CURRENT_GENERAL*usage)+(days*CURRENT_SUPPLY))*PAYONTIMEDISCOUNT,2))
 
 plt.figure(figsize=(25,13))
 plt.title(f"Cost of Current Plan and Alternative Plans Over Time",fontdict= Title_font)
@@ -27,7 +29,7 @@ plt.plot(Dates,current_cost, linewidth = 2)
 with open('master-list-plans.csv','r') as csvfile:
     reader = csv.DictReader(csvfile)
     count = -1
-    legend_names = []
+    legend_names = ["Current Plan"]
     for row in reader:
         count+=1
         Provider = row["Provider"]
